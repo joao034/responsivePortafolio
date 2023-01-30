322e1c8b2f8394919d7e25387e4864be347e296e
joao034 <84355621+joao034@users.noreply.github.com>
2023-01-30 10:11:35 -0500

    Navbar y banner

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('HOla')
+const nav = document.querySelector("nav");
+const toggleBtn = document.createElement("button");
+toggleBtn.innerHTML = "&#9776;";
+
+toggleBtn.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
+
+nav.appendChild(toggleBtn);
